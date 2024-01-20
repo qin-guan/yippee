1c96ba44ed5b468958f95f950eb7b7d6fd74f464
Qin Guan <qinguan20040914@gmail.com>
2024-01-20 12:56:17 +0800

    feat: funny

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { crx } from '@crxjs/vite-plugin'
-import manifest from './manifest.json' // Node 14 & 16
-import manifest from './manifest.json' assert { type: 'json' } // Node >=17
+import manifest from './manifest.config'
 
 // https://vitejs.dev/config/
 export default defineConfig({
