b5dc7c2975017a22f25946670501b9d6324c1fb2
Qin Guan <qinguan20040914@gmail.com>
2024-01-20 12:13:33 +0800

    feat: add crxjs

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { crx } from '@crxjs/vite-plugin'
+import manifest from './manifest.json' // Node 14 & 16
+import manifest from './manifest.json' assert { type: 'json' } // Node >=17
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    crx({ manifest }),
+  ],
 })
